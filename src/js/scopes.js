5c7ba6c7425d05e8731ad25eb6bc37c147b43d8b
Steve Gilberd <steve@erayd.net>
2026-09-24 13:36:55 +1200

    Add localhost scope

--- src/js/scopes.js
+++ src/js/scopes.js
@@ -5,6 +5,8 @@
  */
 export const defaultScope = [
     { match: "^https://", features: ["context", "fill", "http", "passkey"] },
+    // loopback dev servers legitimately run passkey ceremonies (localhost is a secure context)
+    { match: "^https?://(localhost|127\\.0\\.0\\.1|\\[::1\\])[:/]", features: ["context", "fill", "http", "passkey"] },
     { match: "^http://", features: ["context", "fill", "http"] },
     { match: "^(blob|file|ftp)://", features: ["blacklist", "global"] },
     { match: "^(chrome|edge|about):", features: ["blacklist", "global"] },
